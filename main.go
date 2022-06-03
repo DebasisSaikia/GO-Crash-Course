05f84d0f5210a7e28b97601d8f8883df2506e14e
DebasisSaikia <debasishsaikia136@gmail.com>
2022-06-03 16:03:58 +0530

    user input

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main(){
 	// variable
@@ -13,5 +15,30 @@ func main(){
 	fmt.Printf("Welcome to %v app\n", conferenceName);
 	fmt.Println("We have total of",conferenceTicket, "tickets and",remainingTickets,"are remaining")
 	fmt.Println("Get your tickets here to attend");
+
+	// data types in go
+	var firstName string
+	var lastName string
+	var email string
+	var userTicket int
+
+
+	// type of
+	// fmt.Printf("conference name is %T\n",conferenceName)
+
+	// reading user input
+	fmt.Println("Enter you first name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets")
+	fmt.Scan(&userTicket)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets .Email is send to %v",firstName,lastName,userTicket,email)
+
 
 }
