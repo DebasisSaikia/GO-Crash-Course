81c85d695a73d8a4af1dc55ddf30d075448055c6
DebasisSaikia <debasishsaikia136@gmail.com>
2022-06-03 14:28:01 +0530

    template string

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Print("Hello world");
+	// variable
+	var conferenceName ="Go Booking"
+	// constant
+	const conferenceTicket=50;
+
+	var remainingTickets=50;
+
+	fmt.Printf("Welcome to %v app\n", conferenceName);
+	fmt.Println("We have total of",conferenceTicket, "tickets and",remainingTickets,"are remaining")
+	fmt.Println("Get your tickets here to attend");
+
 }
